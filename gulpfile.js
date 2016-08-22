80f27808de805ee1b7e77bd4b04b8b1e6306bb07
Michael Laccetti <michael@laccetti.com>
2016-08-22 18:16:24 -0400

    Migrating the monorepo into different projects; this is glint-cluster

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,48 @@
-const hub = require('gulp-hub');
+// gulp
+const gulp = require('gulp');
 
-// dashboard: 'glint-dashboard/gulpfile.js',
-const projects = {
-  library: 'glint-lib/gulpfile.js',
-  server: 'glint-server/gulpfile.js'
-};
+// plugins
+const eslint = require('gulp-eslint');
 
-hub([projects.dashboard, projects.library, projects.server]);
+// testing
+const mocha = require('gulp-mocha');
+
+// utilities
+const del = require('del');
+
+gulp.task('clean', () => {
+  return del(['dist/**']);
+});
+
+gulp.task('lint', () => {
+  return gulp.src(['src/**/*.js', 'test/**/*.js', 'gulpfile.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', ['lint'], () => {
+  return gulp.src('./test/**/*.js')
+    .pipe(mocha({
+      reporter: 'spec',
+      quiet: false,
+      colors: true,
+      timeout: 10000
+    }));
+});
+
+gulp.task('build', ['test'], () => {
+  // commented out, since we don't do any transpilation or concatenation or minification or whatever (yet)
+  // return gulp.src('src/**/*.js')
+  //  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('src/**/*.js', ['build']);
+});
+
+gulp.task('default', (callback) => {
+  runSequence('clean', 'build', callback);
+});
+
+gulp.task('dist', ['test']);
