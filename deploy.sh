ffe5b0f80663332694c0432b7bb6066ec699a57a
Michael Laccetti <michael@laccetti.com>
2016-08-27 17:07:29 -0400

    Looks like we can multi-tag and just push twice [skip ci]

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,19 @@ echo "Deploying Glint Cluster ${PACKAGE_VERSION} to npm"
 npm publish
 
 echo "Building Glint Master ${PACKAGE_VERSION}"
-docker build -f Dockerfile.master -t quay.io/glint/glint-cluster-master:$PACKAGE_VERSION .
+docker build -f Dockerfile.master -t quay.io/glint/glint-cluster-master:latest -t quay.io/glint/glint-cluster-master:$PACKAGE_VERSION .
 
 echo "Building Glint Worker ${PACKAGE_VERSION}"
-docker build -f Dockerfile.worker -t quay.io/glint/glint-cluster-worker:$PACKAGE_VERSION .
+docker build -f Dockerfile.worker -t quay.io/glint/glint-cluster-worker:latest -t quay.io/glint/glint-cluster-worker:$PACKAGE_VERSION .
 
 echo "Pushing Glint Master ${PACKAGE_VERSION} to quay.io"
 docker push quay.io/glint/glint-cluster-master:$PACKAGE_VERSION
 
+echo "Pushing Glint Master Latest to quay.io"
+docker push quay.io/glint/glint-cluster-master:latest
+
 echo "Pushing Glint Worker ${PACKAGE_VERSION} to quay.io"
 docker push quay.io/glint/glint-cluster-worker:$PACKAGE_VERSION
 
-echo "Tagging Glint Master ${PACKAGE_VERSION} as latest on quay.io"
-docker tag quay.io/glint/glint-cluster-master:$PACKAGE_VERSION quay.io/glint/glint-cluster-master:latest
-
-echo "Tagging Glint Worker ${PACKAGE_VERSION} as latest on quay.io"
-docker tag quay.io/glint/glint-cluster-worker:$PACKAGE_VERSION quay.io/glint/glint-cluster-worker:latest
+echo "Pushing Glint Worker Latest to quay.io"
+docker push quay.io/glint/glint-cluster-worker:latest
